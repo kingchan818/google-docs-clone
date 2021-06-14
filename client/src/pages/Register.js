2999f17fb99994f19967e93e56f79933ab64f4aa
calvin <chancch70@gmail.com>
2021-06-14 16:59:41 +0800

    register page redesign

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { register } from '../redux/action/userAction';
 import styled from 'styled-components';
-import { TextField, Button, Checkbox, FormControlLabel } from '@material-ui/core';
+import { TextField, Button, Checkbox, FormControlLabel, Container, Grid, makeStyles } from '@material-ui/core';
 
 export default function Register() {
     const dispatch = useDispatch();
@@ -31,46 +31,66 @@ export default function Register() {
 
     return (
         <form onSubmit={(e) => submitFrom(e)}>
-            <TextField
-                id="standard-basic"
-                label="Email"
-                onChange={(e) => {
-                    setEmail(e.target.value);
-                }}
-            />
-            <TextField
-                id="standard-basic"
-                label="Username"
-                onChange={(e) => {
-                    setUsername(e.target.value);
-                }}
-            />
-            <TextField
-                id="standard-basic"
-                type="password"
-                label="Password"
-                onChange={(e) => {
-                    setPassword(e.target.value);
-                }}
-            />
-            <TextField
-                id="confirmed_password"
-                type="password"
-                label="Confirm Password"
-                onChange={(e) => {
-                    setPassword1(e.target.value);
-                }}
-            />
-            <FormControlLabel
-                value="top"
-                control={<Checkbox color="primary" />}
-                label="show password "
-                labelPlacement="start"
-                onClick={(e) => showPassword(e)}
-            />
-            <Button type="submit" variant="contained" color="default">
-                Submit
-            </Button>
+            <RegisterForm>
+                <Grid container spacing="2" direction="column" justify="center" alignItems="center">
+                    <Grid item xs={12}>
+                        <TextField
+                            id="standard-basic"
+                            label="Email"
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            id="standard-basic"
+                            label="Username"
+                            onChange={(e) => {
+                                setUsername(e.target.value);
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            id="standard-basic"
+                            type="password"
+                            label="Password"
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            id="confirmed_password"
+                            type="password"
+                            label="Confirm Password"
+                            onChange={(e) => {
+                                setPassword1(e.target.value);
+                            }}
+                        />
+                    </Grid>
+                </Grid>
+                <FormControlLabel
+                    value="top"
+                    control={<Checkbox color="primary" />}
+                    label="show password "
+                    labelPlacement="start"
+                    onClick={(e) => showPassword(e)}
+                />
+                <Button type="submit" variant="contained" color="default">
+                    Submit
+                </Button>
+            </RegisterForm>
         </form>
     );
 }
+
+const RegisterForm = styled.div`
+    display: flex;
+    padding: 20rem;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+`;
